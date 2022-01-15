67aceb762c50748e128bc06d1c3ef4da0e3d4163
Vijayalaxmi Kurtakoti <vijayalaxmim846@gmail.com>
2022-01-15 16:24:13 +0530

    Restaurant API

---
+++ API/Restaurant/index.js
@@ -0,0 +1,64 @@
+import express from "express";
+
+import {RestaurantModel} from "../../database/allModels";
+
+const Router=express.Router();
+
+/*
+Route       /
+Des         Get all the restaurant details
+Params      None
+Access      Public
+Method      GET
+*/
+
+Router.get("/",async(req,res)=>{
+    try{
+        const{city}=req.query;
+        const restaurants=await RestaurantModel.find({city});
+        return res.json({restaurants});
+    }catch(error){
+        return res.status(500).json({error:error.message});
+    }
+});
+
+/*
+Route       /
+Des         Get a particular restaurant details based on id
+Params      _id
+Access      Public
+Method      GET
+*/
+
+Router.get("/:_id",async(req,res)=>{
+    try{
+        const{_id}=req.params;
+        const restaurants=await RestaurantModel.find({_id});
+        return res.json({restaurants});
+    }catch(error){
+        return res.status(500).json({error:error.message});
+    }
+});
+
+/*
+Route       /Search
+Des         Get a particular restaurant details based on id
+Params      SearchString
+Access      Public
+Method      GET
+*/
+
+Router.get("/search",async(req,res)=>{
+    try{
+        const{_id}=req.body;
+        const restaurants=await RestaurantModel.find({
+            name:{$regex:searchString,$options:"i"}
+        });
+        return res.json({restaurants});
+    }catch(error){
+        return res.status(500).json({error:error.message});
+    }
+});
+ 
+
+export default Router;
